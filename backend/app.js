8e5ab071d982dfa36ace0af1ce2451f1fc2a936e
Tyler Abbott <tylerabbottthss@gmail.com>
2018-06-21 16:44:29 -0700

    Changed port again.

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3001;
 
 app.get('/', function(req, res){
 	res.send('Server is on');
