7618d8a9022c752e08019d0fa2db508ba77ec21e
Jasonhjkim <jasonhjkim1205@gmail.com>
2018-06-21 18:27:32 -0700

    Edited the initial file

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,14 @@ app.get('/', function(req, res){
 	res.send('Server is on');
 })
 
+app.get('/api', function(req, res) {
+	res.json({
+		"Test": "This is some shit",
+		"Test2" : 3,
+		"Test3" : "other shit",
+	}) 
+})
+
 app.listen(port, function(req, res) {
 	console.log('Server is listening on...' + port);
 })
